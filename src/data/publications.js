d54567d2076055875a49b4bf572c84192bcddec6
JeongJin Kim <jjkim@ou.edu>
2026-07-12 21:44:19 -0500

    Add site links and publications data modules

---
+++ src/data/publications.js
@@ -0,0 +1,48 @@
+export const publications = [
+  {
+    year: 2025, streams: ['ii'],
+    doi: 'https://doi.org/10.1007/s41542-025-00232-3',
+    html: 'Aitken, J. A., Zhu, Z., Baines, J. I., <strong>Kim, J. J.</strong>, Kaplan, S. A., Dalal, R. S., Hassani, J., &amp; Gibson, J. L. (2025). Emotion regulation at work: A micro-randomized trial comparing three cognitive reappraisal interventions. <i>Occupational Health Science, 9</i>, 991&ndash;1021.',
+  },
+  {
+    year: 2025, streams: ['iii'],
+    doi: 'https://doi.org/10.1007/s12144-025-08136-9',
+    html: 'Aitken, J. A., Dalal, R. S., <strong>Kim, J. J.</strong>, &amp; Zhu, Z. (2025). The self-regulation of counterproductive work behavior: A conceptual replication of Aitken et al. (2024). <i>Current Psychology, 44</i>, 15510&ndash;15514.',
+  },
+  {
+    year: 2025, streams: ['ii'],
+    doi: 'https://doi.org/10.1007/s12144-025-07788-x',
+    html: '<strong>Kim, J. J.</strong>, Kaplan, S. A., Aitken, J. A., &amp; Ponce, L. P. (2025). An examination of the daily relationship between job boredom and later burnout and turnover intentions along with mitigating strategies. <i>Current Psychology, 44</i>, 8807&ndash;8822.',
+  },
+  {
+    year: 2025, streams: ['ii'],
+    doi: 'http://dx.doi.org/10.1111/joop.70020',
+    html: 'Zhu, Z., Aitken, J. A., <strong>Kim, J. J.</strong>, Baines, J. I., Kaplan, S. A., Dalal, R. S., &amp; Hassani, J. (2025). The emotion regulation intervention in the workplace: An ecological momentary approach and its impact on employee job performance. <i>Journal of Occupational and Organizational Psychology, 98</i>, e70020.',
+  },
+  {
+    year: 2024, streams: ['i'],
+    doi: 'https://doi.org/10.1093/obo/9780199828340-0327',
+    html: '<strong>Kim, J. J.</strong>, Son, M., &amp; Dalal, R. S. (2024). Situational strength. <i>Oxford Bibliographies in Psychology</i>. New York: Oxford University Press.',
+  },
+  {
+    year: 2024, streams: ['ii', 'iii'],
+    doi: 'https://doi.org/10.1007/s42761-024-00256-y',
+    html: '<strong>Kim, J. J.</strong>, Kaplan, S. A., Aitken, J. A., &amp; Ponce, L. P. (2024). Within-person dynamics of job boredom and counterproductive work behavior: A latent change score modeling approach. <i>Affective Science, 5</i>, 273&ndash;279.',
+  },
+  {
+    year: 2024, streams: ['ii'],
+    doi: 'https://doi.org/10.1111/joop.12502',
+    note: 'Top 10 most-cited paper published by the journal in 2024',
+    html: 'Park, J., Woo, S. E., &amp; <strong>Kim, J. J.</strong> (2024). Attitudes toward artificial intelligence application at work: Scale development and validation. <i>Journal of Occupational and Organizational Psychology, 97</i>(3), 920&ndash;951.',
+  },
+  {
+    year: 2021, streams: ['ii', 'iii'],
+    doi: 'https://doi.org/10.1177/0894845319853879',
+    html: '<strong>Kim, J. J.</strong>, Park, J., Sohn, Y. W., &amp; Lim, J. I. (2021). Perceived overqualification, boredom, and extra-role behaviors: Testing a moderated mediation model. <i>Journal of Career Development, 48</i>(4), 400&ndash;414.',
+  },
+  {
+    year: 2018, streams: [],
+    doi: 'https://doi.org/10.17315/kjhp.2018.23.2.003',
+    html: 'Shin, S. M., Song, Y. S., <strong>Kim, J. J.</strong>, &amp; Oh, J. S. (2018). The relationship between impulsiveness and smartphone addiction among adolescents: Focused on the possible application of delay discounting task. <i>The Korean Journal of Health Psychology, 23</i>(2), 345&ndash;363.',
+  },
+];
---
+++ src/data/site.js
@@ -0,0 +1,3 @@
+export const EMAIL = 'jjkim@ou.edu';
+export const SCHOLAR_URL = 'https://scholar.google.com/citations?user=3312EoQAAAAJ&hl=en';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/jeongjinjjkim/';
